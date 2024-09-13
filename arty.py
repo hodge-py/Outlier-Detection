44a32bb2f41c7fc23e1f3b9f3e0fc39bdf5108b2
hodge-py <105604814+hodge-py@users.noreply.github.com>
2024-09-12 19:46:23 -0600

    c

--- arty.py
+++ arty.py
@@ -7,10 +7,7 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.cluster import KMeans
 
 
-arr = np.random.multivariate_normal([50,45], [[20,0],[0,17]], 1000)
+#arr = np.random.multivariate_normal([50,45], [[20,0],[0,17]], 1000)
 
-print(arr)
 
-plt.scatter(arr[:,0],arr[:,1])
-plt.show()
 
