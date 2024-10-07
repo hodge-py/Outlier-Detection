87cb9560387e5bf2176a9f18eb783a4b0abdeefb
hodge-py <105604814+hodge-py@users.noreply.github.com>
2024-10-07 15:10:04 -0600

    c

--- convertMatlab.py
+++ convertMatlab.py
@@ -4,18 +4,18 @@ import numpy as np
 from mat4py import loadmat
 import matplotlib.pyplot as plt
 
-data = loadmat("wbc.mat")
+data = loadmat("t4_8k.mat")
 print(data)
 df = pd.DataFrame()
 arr = []
 
 count = 1
 for x in data:
-    df = pd.concat([df,pd.DataFrame(data[x])])
+    df = pd.concat([df,pd.DataFrame(data[x])], axis=1)
 
 print(df)
-print(np.count_nonzero(np.array(df.iloc[:,-1:])))
-plt.scatter(df.iloc[:,0],df.iloc[:,2], c=df.iloc[:,-1:], cmap='viridis')
+
+plt.scatter(df.iloc[:,0],df.iloc[:,1])
 plt.show()
 
 
